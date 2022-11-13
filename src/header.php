5c0599df1877a097210f21cc7df1aadbbd256ddc
Pierre Braem <pierrebraem@orange.fr>
2022-11-13 17:57:59 +0100

    FEAT: tester si bootstrap fonctionne

---
+++ src/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PMU - Pièce Mécano à l'Unité</title>
+        <meta charset="utf-8"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/pmu">PMU</a>
+                <button class="navbar-toggler" type="button"></button>
+        </nav>
