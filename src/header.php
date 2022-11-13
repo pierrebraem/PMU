ba07d3bf5404bfa7a97008b52141c96dc328f8c6
Pierre Braem <pierrebraem@orange.fr>
2022-11-13 18:26:02 +0100

    FEAT: programmation de l'entête

--- src/header.php
+++ src/header.php
@@ -6,8 +6,31 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class="navbar navbar-expand-lg bg-primary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="/pmu">PMU</a>
-                <button class="navbar-toggler" type="button"></button>
+                <button class="navbar-toggler" type="button" data-bs-target="#navbarPMU" aria-controls="navbarPMU" aria-expanded="false" aria-label="Toggle navigation"></button>
+                <div class="collapse navbar-collapse" id="navbarPMU">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/pmu/parcourir">Parcourir</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/pmu/conseil">Conseil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/pmu/panier">Panier</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/pmu/inscription">Inscription</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/pmu/connexion">Connexion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/pmu/compte">Mon compte</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
