d61bfc903c9095b7430b58f1880cf8454f5b50d0
Pierre Braem <pierrebraem@orange.fr>
2022-11-15 12:14:57 +0100

    UPDATE: Programmation de la carte "Conseil"

--- Views/accueil.php
+++ Views/accueil.php
@@ -1,5 +1,6 @@
 <?php require 'src/header.php' ?>
 
+<!-- catégorie -->
 <div class="card w-25 ms-3">
   <h5 class="card-header">Catégorie</h5>
   <div class="card-body">
@@ -19,6 +20,28 @@
       <a href="#" class="list-group-item d-flex justify-content-between align-items-center">
         Catégorie 3
         <span class="badge bg-primary round-pill">15</span>
+      </a>
+    </ul>
+  </div>
+</div>
+
+<!-- Conseil -->
+<div class="card w-25 ms-3 mt-3">
+  <h5 class="card-header">Conseil</h5>
+  <div class="card-body">
+    <ul class="list-group">
+      <a href="#" class="list-group-item d-flex justify-content-between align-items-center">
+        Conseil
+      </a>
+    </ul>
+    <ul class="list-group">
+      <a href="#" class="list-group-item d-flex justify-content-between align-items-center">
+        Conseil 2
+      </a>
+    </ul>
+    <ul class="list-group">
+      <a href="#" class="list-group-item d-flex justify-content-between align-items-center">
+        Conseil 3
       </a>
     </ul>
   </div>
