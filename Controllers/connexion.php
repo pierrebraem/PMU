bd64bf5aaa1c5101a6b56634e88d33d62a9bbdb9
Pierre Braem <pierrebraem@orange.fr>
2022-11-22 11:55:02 +0100

    UPDATE: Déclaration de la classe ConnexionC

--- Controllers/connexion.php
+++ Controllers/connexion.php
@@ -1,10 +1,12 @@
 <?php
-    function connexion(){
-        try{
-            return new PDO('mysql:host=localhost;dbname=pmu;charset=utf8', 'admin', 'admin');
-        }
-        catch (Exception $e){
-            die('Erreur: '.$e->getMessage());
+    class ConnexionC{
+        function connexion(){
+            try{
+                return new PDO('mysql:host=localhost;dbname=pmu;charset=utf8', 'admin', 'admin');
+            }
+            catch (Exception $e){
+                die('Erreur: '.$e->getMessage());
+            }
         }
     }
 ?>
