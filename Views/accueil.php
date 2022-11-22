28ae022e8af6c3c8965b81932a8b169aa8221410
Pierre Braem <pierrebraem@orange.fr>
2022-11-22 10:35:10 +0100

    UPDATE: première version de la page d'accueil

--- Views/accueil.php
+++ Views/accueil.php
@@ -1,15 +1,40 @@
 <?php require 'src/header.php' ?>
 
-<!-- barre de recherche -->
-<div class="card w-25 ms-3">
-  <h5 class="card-header">Recherche</h5>
-  <div class="card-body">
-    <form class="form-inline">
-      <input class="from-control" type="search" placeholder="Rechercher" aria-label="Recherche">
-      <button class="btn btn-outline-success" type="submit">Ok</button>
-    </form>
+<!-- barre de recherche et promotion et dernières ventes -->
+<div class="container ms-2 mt-3 w-100">
+  <div class="row">
+    <div class="col-lg-4">
+      <div class="card">
+        <h5 class="card-header">Recherche</h5>
+        <div class="card-body">
+          <form class="form-inline">
+            <input class="from-control" type="search" placeholder="Rechercher" aria-label="Recherche">
+            <button class="btn btn-outline-success" type="submit">Ok</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 ms-5">
+      <div class="card h-100">
+        <div class="card-body">
+          Afficher les promotions
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 ms-5">
+      <div class="card h-100">
+        <h5 class="card-header">Dernières ventes</h5>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Vente n°1</li>
+          <li class="list-group-item">Vente n°2</li>
+          <li class="list-group-item">Vente n°3</li>
+          <li class="list-group-item">Vente n°4</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </div>
+
 
 <!-- Catégorie -->
 <div class="card w-25 ms-3 mt-3">
