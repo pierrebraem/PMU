00101367a1e83aade51e8855737b310a54dd5b33
Pierre Braem <pierrebraem@orange.fr>
2022-12-01 11:28:05 +0100

    UPDATE: mise à jour de bootstrap

--- src/header.php
+++ src/header.php
@@ -3,7 +3,7 @@
     <head>
         <title>PMU - Pièce Mécano à l'Unité</title>
         <meta charset="utf-8"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
     <body>
         <nav class="navbar navbar-expand-lg bg-primary">
