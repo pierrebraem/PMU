8742228f3143331a5c4fee5c49fe02a9c9684a40
Pierre Braem <pierrebraem@orange.fr>
2022-12-02 16:49:21 +0100

    FEAT: Mise en page de conseil

--- Views/conseil.php
+++ Views/conseil.php
@@ -1,5 +1,35 @@
 <?php require 'src/header.php' ?>
 
-<p>Conseil</p>
+<div class="col d-flex justify-content-center">
+    <div class="card w-75 text-center">
+        <h5 class="card-header">Recherche</h5>
+        <div class="card-body">
+            <form class="form-inline">
+                <input class="from-control" type="search" placeholder="Rechercher" aria-label="Recherche">
+                <button class="btn btn-outline-success" type="submit">Ok</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="col d-flex justify-content-center mt-5">
+    <div class="card w-75 text-center">
+        <h5 class="card-header">Conseil 1</h5>
+        <div class="card-body">
+            <p>Conseil 1</p>
+            <p>Description conseil 1</p>
+        </div>
+    </div>
+</div>
+
+<div class="col d-flex justify-content-center mt-5">
+    <div class="card w-75 text-center">
+        <h5 class="card-header">Conseil 2</h5>
+        <div class="card-body">
+            <p>Conseil 2</p>
+            <p>Description conseil 2</p>
+        </div>
+    </div>
+</div>
 
 <?php require 'src/footer.php' ?>
