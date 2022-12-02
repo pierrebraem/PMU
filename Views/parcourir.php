7adfd86769160ffef0a7f105ad992fd383d40e1a
Pierre Braem <pierrebraem@orange.fr>
2022-12-02 16:41:19 +0100

    FEAT: Mise en page de parcourir

--- Views/parcourir.php
+++ Views/parcourir.php
@@ -1,5 +1,39 @@
 <?php require 'src/header.php' ?>
 
-<p>Parcourir</p>
+<div class="col d-flex justify-content-center">
+    <div class="card w-75 text-center">
+        <h5 class="card-header">Recherche</h5>
+        <div class="card-body">
+            <form class="form-inline">
+                <input class="from-control" type="search" placeholder="Rechercher" aria-label="Recherche">
+                <button class="btn btn-outline-success" type="submit">Ok</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="col d-flex justify-content-center mt-5">
+    <div class="card w-75 text-center">
+        <h5 class="card-header">Nom produit 1</h5>
+        <div class="card-body">
+            <p>Nom produit 1</p>
+            <p>Description produit 1</p>
+            <p>9.99€</p>
+            <button class="btn btn-success"><i class="fa-solid fa-cart-shopping"></i></button>
+        </div>
+    </div>
+</div>
+
+<div class="col d-flex justify-content-center mt-5">
+    <div class="card w-75 text-center">
+        <h5 class="card-header">Nom produit 2</h5>
+        <div class="card-body">
+            <p>Nom produit 2</p>
+            <p>Description produit 2</p>
+            <p>14.99€</p>
+            <button class="btn btn-success"><i class="fa-solid fa-cart-shopping"></i></button>
+        </div>
+    </div>
+</div>
 
 <?php require 'src/footer.php' ?>
