40c758362e50e831057bf275e3e4170797ad2f84
Pierre Braem <pierrebraem@orange.fr>
2022-12-09 15:40:19 +0100

    FEAT: Création script conseil

---
+++ BDD/scriptCreationConseil.sql
@@ -0,0 +1,21 @@
+INSERT INTO conseil (nom, description, image) VALUES
+("montage1", "montage1", "./src/images/montages/montage1.png"),
+("montage2", "montage2", "./src/images/montages/montage2.jpg");
+
+INSERT INTO conseil_produit(id_conseil, id_produit, quantite) VALUES
+(1, 1, 1),
+(1, 2, 1),
+(1, 13, 1),
+(1, 14, 1),
+(1, 15, 1),
+(1, 16, 1),
+(1, 17, 1),
+(1, 18, 1),
+(1, 19, 1),
+(1, 20, 1),
+(1, 21, 1),
+(1, 22, 1),
+(1, 41, 1),
+(2, 46, 3),
+(2, 23, 1),
+(2, 34, 4);
