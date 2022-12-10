223ce95a8c9415372d60b8db2c49b87da9fe6172
Pierre Braem <pierrebraem@orange.fr>
2022-12-10 15:52:30 +0100

    FEAT: Design panier

--- Views/panier.php
+++ Views/panier.php
@@ -1,5 +1,72 @@
 <?php require 'src/header.php' ?>
 
-<p>Panier</p>
+<section class="h-100">
+    <div class="container py-3">
+        <div class="row d-flex justify-content-center">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Panier</h5>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            <img src="#">
+                        </div>
+                        <div class="col">
+                           <p>Nom : montBC6</p>
+                           <p>Description : montBC6</p>
+                           <p>Catégorie : Test</p>
+                        </div>
+                        <div class="col">
+                            <p>Quantité : 3</p>
+                        </div>
+                        <div class="col">
+                            <p>14,99€</p>
+                        </div>
+                        <div class="col">
+                            <a href="#" class="btn btn-danger">Supprimer</a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <img src="#">
+                        </div>
+                        <div class="col">
+                           <p>Nom : montBC13</p>
+                           <p>Description : montBC13</p>
+                           <p>Catégorie : Test</p>
+                        </div>
+                        <div class="col">
+                            <p>Quantité : 2</p>
+                        </div>
+                        <div class="col">
+                            <p>9,99€</p>
+                        </div>
+                        <div class="col">
+                            <a href="#" class="btn btn-danger">Supprimer</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="pt-3">
+                <div class="card">
+                    <div class="card-header">
+                        <h5>Prix total</h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <p>Prix : 24,98€</p>
+                            </div>
+                            <div class="col">
+                                <a href="#" class="btn btn-success">Payer</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php require 'src/footer.php' ?>
